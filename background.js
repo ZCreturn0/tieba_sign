a84812047103cc1578cbe0c0ac8a6b03445a25b7
zc_return0 <237350543@qq.com>
2019-11-04 16:46:50 +0800

    获取tbs参数

--- background.js
+++ background.js
@@ -1,5 +1,8 @@
 let params = [];
 let page = 1;
+let tbs = '';
+
+// 获取所有关注的贴吧
 function getAllTiebaInfo(){
     $.get(`http://tieba.baidu.com/f/like/mylike?&pn=${page}`, (data) => {
         let html = $.parseHTML(data);
@@ -15,11 +18,19 @@ function getAllTiebaInfo(){
             getAllTiebaInfo();
         }
         else{
-            aaa();
+            getTbs();
         }
     });
 }
-getAllTiebaInfo();
-function aaa(){
-    console.log(params);
+
+function getTbs(){
+    // 所有贴吧签到都需要 tbs 参数,且所有所需的 tbs 参数都是一样的,所以获取一个贴吧的 tbs 参数即可
+    let info = params[0];
+    let url = `http://tieba.baidu.com${info.link}`;
+    $.get(url, (data) => {
+        let index = data.search(/'tbs':/);
+        tbs = data.substr(index + 8, 26);
+    });
 }
+
+getAllTiebaInfo();
