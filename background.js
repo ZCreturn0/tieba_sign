7911448ea391055df742fb7ba3bb80f2161a42ea
zc_return0 <237350543@qq.com>
2019-11-05 17:40:19 +0800

    使用Promise

--- background.js
+++ background.js
@@ -1,5 +1,19 @@
 let params = [];
-let page = 1;
+// 测试时设成 5
+let page = 5;
+
+// 错误码
+const errorCode = {
+    '0': '签到成功',
+    '1101': '重复签到',
+    '1010': '贴吧不存在'
+};
+// 统计签到结果
+const counter = {
+    '签到成功': 0,
+    '重复签到': 0,
+    '贴吧不存在': 0
+};
 
 // 把请求设成同步
 $.ajaxSettings.async = false;
@@ -31,35 +45,74 @@ function getAllTiebaInfo(){
 }
 
 // 获取 tbs 参数
-function getTbs(){
+async function getTbs(){
+    // 存储所有的 Promise
+    let promises = [];
     // 所有贴吧签到都需要 tbs 参数
     for (let info of params) {
         // 获取每个贴吧的 tbs 参数
         let url = `http://tieba.baidu.com${info.link}`;
-        $.get(url, (data) => {
-            let index = data.search(/'tbs':/);
-            let tbs = data.substr(index + 8, 26);
-            // 传入关键字和 tbs 进行签到
-            sign(info.kw, tbs);
-        });
+        await fetch(url)
+        .then(data => {
+            data.text()
+            .then(text => {
+                let index = text.search(/'tbs':/);
+                let tbs = text.substr(index + 8, 26);
+                promises.push(sign(info.kw, tbs));
+            })
+        })
+        // let promise = fetch(url);
+        // promises.push(promise);
+        // $.get(url, (data) => {
+        //     let index = data.search(/'tbs':/);
+        //     let tbs = data.substr(index + 8, 26);
+        //     // 传入关键字和 tbs 进行签到
+        //     sign(info.kw, tbs);
+        // });
     }
+    // console.log(promises);
+    Promise.all(promises)
+    .then(async datas => {
+        datas.forEach(async data => {
+            await data.json()
+            .then(json => {
+                console.log(json);
+                counter[errorCode[json.no]]++;
+            })
+        })
+        console.log(counter);
+    })
+    .catch(err => {
+        console.log(err);
+    })
 }
 
-// 签到
+// 签到,返回 promise
 function sign(kw, tbs){
-    $.post("http://tieba.baidu.com/sign/add", {
-        ie: 'utf-8',
-        kw,
-        tbs
-    }, (data) => {
-        let json = JSON.parse(data);
-        if (!json.no) {
-            console.log(`贴吧 ${kw} 签到成功!!`);
-        }
-        else{
-            console.log(`贴吧 ${kw}: ${json.error}`);
-        }
-    })
+    let formData = new FormData();
+    formData.append('ie', 'utf-8');
+    formData.append('kw', kw);
+    formData.append('tbs', tbs);
+    return fetch("http://tieba.baidu.com/sign/add", {
+        method: 'POST',
+        mode: 'cors',
+        body: formData
+    });
+
+    
+    // $.post("http://tieba.baidu.com/sign/add", {
+    //     ie: 'utf-8',
+    //     kw,
+    //     tbs
+    // }, (data) => {
+    //     let json = JSON.parse(data);
+    //     if (!json.no) {
+    //         console.log(`贴吧 ${kw} 签到成功!!`);
+    //     }
+    //     else{
+    //         console.log(`贴吧 ${kw}: ${json.error}`);
+    //     }
+    // })
 }
 
 getAllTiebaInfo();
