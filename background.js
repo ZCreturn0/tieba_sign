3541c4cd8df4f12b1d624bebbd61a525ca56549a
zc_return0 <237350543@qq.com>
2019-11-06 16:45:06 +0800

    增加签到判断,增加签到完成提示

--- background.js
+++ background.js
@@ -4,6 +4,31 @@ let page = 1;
 // 把请求设成同步
 $.ajaxSettings.async = false;
 
+// 格式化时间
+function dateFormat(fmt, date) {
+    let ret;
+    let opt = {
+        "Y+": date.getFullYear().toString(),
+        "m+": (date.getMonth() + 1).toString(),
+        "d+": date.getDate().toString(),
+        "H+": date.getHours().toString(),
+        "M+": date.getMinutes().toString(),
+        "S+": date.getSeconds().toString()
+    };
+    for (let k in opt) {
+        ret = new RegExp("(" + k + ")").exec(fmt);
+        if (ret) {
+            fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+        };
+    };
+    return fmt;
+}
+
+// 判断是否同一天
+function isSameDay(dateA, dateB) {
+    return (dateA.setHours(0, 0, 0, 0) == dateB.setHours(0, 0, 0, 0));
+}
+
 // 获取所有关注的贴吧
 function getAllTiebaInfo() {
     // 获取 page 页的所有贴吧
@@ -51,6 +76,22 @@ function getTbs() {
         });
     }
     console.log(`成功签到了 ${signed} 个贴吧;重复签到 ${resigned} 个贴吧;因其他原因签到失败 ${other} 个贴吧.`);
+    let str = [];
+    str.push(`共关注了 ${params.length} 个贴吧`);
+    signed ? str.push(`成功签到了 ${signed} 个贴吧`) : '';
+    resigned ? str.push(`重复签到 ${resigned} 个贴吧;`) : '';
+    other ? str.push(`因其他原因签到失败 ${other} 个贴吧`) : '';
+    chrome.notifications.create(null, {
+        type: 'basic',
+        iconUrl: 'img/tieba.png',
+        title: '签到结果',
+        message: str.join('\n')
+    });
+    chrome.storage.sync.set({
+        last_sign_time: dateFormat('YYYY-mm-dd HH:MM:SS', new Date())
+    }, () => {
+        console.log('已更新');
+    });
 }
 
 // 签到
@@ -74,4 +115,21 @@ function sign(kw, tbs) {
     })
 }
 
-getAllTiebaInfo();
+// getAllTiebaInfo();
+
+// 获取上次签到的时间
+chrome.storage.sync.get('last_sign_time', (res) => {
+    if (res && res.last_sign_time) {
+        let now = new Date();
+        let last_sign_time = new Date(res.last_sign_time);
+        if (isSameDay(now, last_sign_time)) {
+            console.log("今天已经签到过了!");
+        }
+        else{
+            getAllTiebaInfo();
+        }
+    }
+    else{
+        getAllTiebaInfo();
+    }
+})
